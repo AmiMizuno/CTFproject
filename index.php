5a6f822ef3d1ece423379229b7c0f2d13de3914e
sterhnet <yur-platono@mail.ru>
2015-07-02 11:07:12 +0700

    sample_SQL

--- index.php
+++ index.php
@@ -73,6 +73,28 @@ if($b)
 else
 	$a=$n-$p+$bb;
 echo "Hello world!",$a;
+$link = mysql_connect('127.0.0.1', 'root', '')
+    or die('Не удалось соединиться: ' . mysql_error());
+echo 'Соединение успешно установлено';
+mysql_select_db('ctfbd') or die('Не удалось выбрать базу данных');
+
+// Выполняем SQL-запрос
+$query = 'SELECT * FROM users';
+$result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
+
+// Выводим результаты в html
+echo "<table>\n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Освобождаем память от результата
+mysql_free_result($result);
 
 ?>
 <p><center><img src="2.gif" alt="minyons"></center></p>
