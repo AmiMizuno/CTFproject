30ee2a4de8c5919a62e56b67c7c90aed7b16a9dd
Мария <strannik.ezi@mail.ru>
2015-07-03 11:40:18 +0700

    profile

---
+++ profile.php
@@ -0,0 +1,37 @@
+﻿<html>
+<body bgcolor="#641c10">
+<p><center><img src="1.png" alt="mikky"></center></p>
+<font color="#fff2b4"><p><center><h1>ТАВЕРНА МО</h1></center></p></font>
+
+<p><center><table>
+ <tr><td><img src="br.png" alt="mikky"></td>
+ <td>
+<p><center><font color="#fff2b4"><h3>Профиль</h3>
+ <table width="300" cellspacing="2"  border="2" bgcolor="#fffcff">
+				 <tr>
+				   <td><b>Логин</b></td>
+				   <td>иуиуиуиуиуиуиу</td>
+				 </tr>
+				 <tr>
+				  <td> <b>Почта</b></td>
+				   <td>уруоуооуоуоуоо</td>
+				 </tr>
+				 <tr>
+				   <td><b>О себе</b></td>
+				   <td>улоурлорулорул</td>
+				 </tr>
+				 
+			   </table>
+
+   </center></p>
+   </td>
+   </table>
+     </center></p>
+<?php
+
+	
+
+?>
+
+
+</html>
