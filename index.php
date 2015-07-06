02230082fb4101a24116694d68a9ba44f062dade
Мария <strannik.ezi@mail.ru>
2015-07-06 16:23:58 +0700

    glavn2

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 ﻿<html>
-<p><center><body background="rrr.jpg">
- <table style="width: 100%;height: 100%" border="0">
+<p><center><body background="rr5.jpg">
+ <table style="border-radius: 8px; width: 100%;height: 100%" border="0">
    <tr>
    <td  
-  style="width: 15%; height: 100%" ><img src="1234.png"></th></td>
+  style="width: 15%; height: 100%"><img src="1234.png"><br><br><br><img src="2345.png"><img src="234.png"></th></td>
 
-    <th style="height: 100%"bgcolor="#641c10" border="2"<img src="1.png">
+    <th>
+	<table style="border-radius: 20px; border-color: #000000; width: 100%;height: 100%" border="0" bgcolor="#641c10"><th style="height: 100%" >
 <font color="#fff2b4"><img src="1.png" alt="mikky">
 <h1>ONLINE БАР</h1></font>
 <img src="menu.png" usemap="#map1" border="0">
@@ -27,8 +28,8 @@
 				<area shape="rect" coords="640,30, 720,70"
                 href="#" alt="выход">
 		
-      </map><br><br><br><br><br><br><br><br>d<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></th>
-	<td style="width: 15%; height: 100%" > >Ячейка 2</td>
+      </map><br><br><br><br><br><br><br><br>d<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></th></table></th>
+	<td style="width: 15%; height: 100%" ><img src="67.png"><br><br><br><img src="76.png"><img src="678.png"></td>
    </tr>
  </table></center></p>
 
