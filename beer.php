a56f584e6877cc4ffb7a397ade155402a7a07f07
Мария <strannik.ezi@mail.ru>
2015-07-06 09:40:29 +0700

    menu_vezde

--- beer.php
+++ beer.php
@@ -2,6 +2,26 @@
 <body bgcolor="#641c10">
 <p><center><img src="1.png" alt="mikky"></center></p>
 <font color="#fff2b4"><p><center><h1>ONLINE БАР</h1></font>
+<img src="menu.png" usemap="#map1" border="0">
+      <map name="map1">  
+	  <area shape="rect" coords="110,20, 200,192"
+                href="#" alt="главная">
+	  <area shape="rect" coords="210,80, 300,192"
+                href="reg.php" alt="регистрация">
+		<area shape="rect" coords="320,20, 400,192"
+                href="orders.php" alt="заказы">
+		<area shape="rect" coords="410,20, 450,82"
+                href="beer.php" alt="пиво">
+		<area shape="rect" coords="420,80, 510,192"
+                href="catalog.php" alt="каталог">
+          <area shape="rect" coords="521,20, 620,192"
+                href="profile.php" alt="профиль">
+		  <area shape="rect" coords="640,90, 720,192"
+                href="login.php" alt="вход">
+				<area shape="rect" coords="640,30, 720,70"
+                href="#" alt="выход">
+		
+      </map>
 
 <p><center><img src="mmm.jpg"></center></p>
 <font color="#ef6900"><p><center><h1>М-м-м… пиво…</center></p></h1>
