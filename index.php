371142063622c3ad76c1a0e56f442c69fc3e6129
Мария <strannik.ezi@mail.ru>
2015-07-06 12:24:55 +0700

    index v.0.1

--- index.php
+++ index.php
@@ -1,104 +1,37 @@
 ﻿<html>
-<body bgcolor="#fff2b4">
-<p><center><img src="1.png" alt="mikky"></center></p>
-<font color="#641c10"><p><center><h1>PROJECT</h1></center></p></font>
-
-
-
- <p><center><form name="forma1">
-     <input type="text" name="LOGIN" size="40"
-            maxlength="40" value="	LOGIN">
-<input type="text" name="PAROL" size="40"
-            maxlength="40" value="	PAROL">
-  <input type="submit" name="submit" value="ОТПРАВИТЬ">
-</form>
-
-
-
-
-
-<form name="forma1">
-    
- <table  border="0" cellspacing="5" cellpadding="5">
-
-  <font color="#ef6900"><h2>Регистрация</h2></font>
-
+<p><center><body background="rrr.jpg">
+ <table style="width: 100%;height: 100%" border="0">
    <tr>
-    <td align="right" valign="top">Логин</td>
-    <td><input type="text" name="name" size="40"></td>
+   <td  
+  style="width: 15%; height: 100%" ><img src="1234.png"></th></td>
+
+    <th style="height: 100%"bgcolor="#641c10" border="2"<img src="1.png">
+<font color="#fff2b4"><img src="1.png" alt="mikky">
+<h1>ONLINE БАР</h1></font>
+<img src="menu.png" usemap="#map1" border="0">
+      <map name="map1">  
+	  <area shape="rect" coords="110,20, 200,192"
+                href="index.php" alt="главная">
+	  <area shape="rect" coords="210,80, 300,192"
+                href="reg.php" alt="регистрация">
+		<area shape="rect" coords="320,20, 400,192"
+                href="orders.php" alt="заказы">
+		<area shape="rect" coords="410,20, 450,82"
+                href="beer.php" alt="пиво">
+		<area shape="rect" coords="420,80, 510,192"
+                href="catalog.php" alt="каталог">
+          <area shape="rect" coords="521,20, 620,192"
+                href="profile.php" alt="профиль">
+		  <area shape="rect" coords="640,90, 720,192"
+                href="login.php" alt="вход">
+				<area shape="rect" coords="640,30, 720,70"
+                href="#" alt="выход">
+		
+      </map><br><br><br><br><br><br><br><br>d<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></th>
+	<td style="width: 15%; height: 100%" > >Ячейка 2</td>
    </tr>
+ </table></center></p>
 
-   <tr>
-    <td align="right" valign="top">e-mail</td>
-    <td><input type="text" name="e-mail" size="40"></td>
-   </tr>
-
-   <tr>
-    <td align="right" valign="top" >Пароль</td>
-    <td>
-     <input type="password" name="password" size="40">
-    </td>
-   </tr>
-
-   <tr>
-    <td align="right" valign="top" >Повтор пароля</td>
-    <td>
-     <input type="password" name="password2" size="40">
-    </td>
-   </tr>
-   <tr>
-    <td align="right" valign="top">О себе</td>
-    <td>
-     <input type="info" name="info" size="40">
-    </td>
-   </tr>
-   <tr>
-    <td align="right" colspan="2">
-     <input type="submit" name="submit" value="Отправить">
-     <input type="reset" name="reset" value="Очистить">
-    </td>
-   </tr>
-
- </table>
-
-</form>
-
-   </center></p>
-<?php
-
-$link = mysql_connect('127.0.0.1', 'root', '')
-    or die('Не удалось соединиться: ' . mysql_error());
-mysql_select_db('ctfbd') or die('Не удалось выбрать базу данных');
-if(count($_GET)>0)
-{
-$name=$_GET["name"];
-$mail=$_GET["e-mail"];
-$info=$_GET["info"];
-$passwd=$_GET["password2"];
-$query = "SELECT * FROM users WHERE login='$name'";
-$result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
-
-
-$i=0;
-while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) 
-    foreach ($line as $col_value)
-		$i+=1;
-
-
-if($i==0)
-{
-	mysql_free_result($result);
-	$query = "INSERT INTO users(login, password,email,info) VALUES ('$name','$passwd','$mail','$info')";
-	$result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
-	echo "Пользователь ", $name," успешно зарегестрирован.";
-}
-else
-	{
-		mysql_free_result($result);
-		echo "Логин занят";
-	}
-}
-?>
 
 
 </html>
