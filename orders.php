54b3a5cfec1d0aed53b12cc64adeb5c19162c8c6
sterhnet <yur-platono@mail.ru>
2015-07-06 10:02:42 +0700

    orders without auth

--- orders.php
+++ orders.php
@@ -27,7 +27,29 @@
                 href="#" alt="выход">
 		
       </map></center></p>
-<p><center><table>
+	  
+	  
+	  
+<?php
+
+if (isset($_COOKIE['userid']))   
+{
+
+$link = mysql_connect('127.0.0.1', 'root', '')
+    or die('Не удалось соединиться: ' . mysql_error());
+mysql_select_db('ctfbd') or die('Не удалось выбрать базу данных');
+$us=$_COOKIE['userid'];
+$query = "SELECT * FROM users WHERE info2='$us'";
+$result = mysql_query($query) or die('Запрос не удался: '. mysql_error());
+$i=0;
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) 
+    {
+		$i+=1;
+		
+	}
+if($i>0)
+{
+	echo '<p><center><table>
  
  <td>
 <p><center><font color="#fff2b4"><img src="df1.png">
@@ -42,7 +64,7 @@
 		
     </tr>
     <tr>
-        <td> <input type="checkbox" name="wsk1" value="yes1"></td><td>Виски «Chivas Regal»</td><td> <input type="number" name="chival" id="dva" pattern="^[ 0-9]+$"   value="y1"></td><td>4000</td>
+        <td> <input type="checkbox" name="wsk1" value="yes1"></td><td>Виски «Chivas Regal»</td><td> <input type="number" name="chival" id="dva" pattern="^[ 0-9]+$"   value="y1" min="0"></td><td>4000</td>
     </tr>
     <tr>
         <td> <input type="checkbox" name="vodka" value="yes2"></td><td>Водка «Абсолют»</td><td> <input type="number" min="0" name="absol" id="dva" pattern="^[ 0-9]+$" value="y2"></td><td>1149</td>
@@ -73,26 +95,7 @@
    </center></p>
    </td>
    </table>
-     </center></p>
-
-<?php
-
-if (isset($_COOKIE['userid']))   
-{
-$link = mysql_connect('127.0.0.1', 'root', '')
-    or die('Не удалось соединиться: ' . mysql_error());
-mysql_select_db('ctfbd') or die('Не удалось выбрать базу данных');
-$us=$_COOKIE['userid'];
-$query = "SELECT * FROM users WHERE info2='$us'";
-$result = mysql_query($query) or die('Запрос не удался: '. mysql_error());
-$i=0;
-while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) 
-    {
-		$i+=1;
-		
-	}
-if($i>0)
-{
+     </center></p>';
 	if(isset($_REQUEST['wsk1'])||isset($_REQUEST['vodka'])||isset($_REQUEST['wsk2'])||isset($_REQUEST['martini'])||isset($_REQUEST['liker'])||isset($_REQUEST['jack']))
 	{
 	$chivas=0;
@@ -122,10 +125,13 @@ echo "<h2>Заказ успешно оформлен!</h2>";
 }		
 }
 else
-echo "<h2>Вы не авторизованы!</h2>";
+echo '<p align="center" style="color:#fff2b4;font-size:25pt" >Вы не авторизованы!</p>';
 }
 else
-echo "<h2>Вы не авторизованы!</h2>";
+echo '<p align="center" style="color:#fff2b4;font-size:25pt" >Вы не авторизованы!</p>';
 ?>
+
+
+
 
 </html>
