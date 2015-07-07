5659875b6e92fa39686dffdfe96745a708ee83c7
Мария <strannik.ezi@mail.ru>
2015-07-07 11:54:37 +0700

    catalog

--- catalog.php
+++ catalog.php
@@ -34,21 +34,23 @@
 <p><center><img src="15.png" usemap="#map" border="0">
       <map name="map">  
 		   <area shape="rect" coords="360,0, 250,192"
-                href="#" alt="виски1">
+                href="?a=cr" alt="виски1">
 	      <area shape="rect" coords="420,0, 370,192"
-                href="#" alt="водка">
+                href="?a=va" alt="водка">
 		  <area shape="rect" coords="520,0, 430,192"
-                href="#" alt="виски2">
+                href="?a=jw" alt="виски2">
           <area shape="rect" coords="530,0, 590,192"
-                href="#" alt="мартини">
+                href="?a=mb" alt="мартини">
           <area shape="rect" coords="591,0, 640,192"
-                href="#" alt="ликер">
+                href="?a=lk" alt="ликер">
 		  <area shape="rect" coords="660,0, 720,192"
-                href="#" alt="джек">
+                href="?a=jd" alt="джек">
 		
       </map>
+<?php
+if (isset($_GET['a'])) echo '<img src="',$_GET['a'],'.png">';
 
-
+?>
 </center></p></th></table></th><td style="width: 15%; height: 100%; vertical-align: top;" ><img src="67.png"><br><br><br><img src="76.png"><img src="678.png"></td>
    </tr>
  </table></center></p>
